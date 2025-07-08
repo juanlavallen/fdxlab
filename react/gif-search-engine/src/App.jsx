bb5ee794c06b784b4262587df32db9ea36869df2
Juan Lavallen <juan.lavallen.dev@gmail.com>
2025-07-07 23:13:23 -0300

    chore: remove obsolete App.jsx file

--- react/gif-search-engine/src/App.jsx
+++ react/gif-search-engine/src/App.jsx
@@ -1,19 +1,22 @@
-import { useState } from "react"
+import { useState } from "react";
+import { InputSearch } from "./components/InputSearch";
 
 function App() {
-  const [categories, setCategories] = useState(['Dragón Ball Z']);
+  const [categories, setCategories] = useState(["Dragón Ball Z"]);
 
   return (
     <>
       <h1>Search Gifs</h1>
+
+      <InputSearch />
+
       <ol>
         {categories.map((category, index) => {
-          return <li key={index}>{category}</li>
+          return <li key={index}>{category}</li>;
         })}
       </ol>
     </>
-  )
-
+  );
 }
 
-export default App
+export default App;
